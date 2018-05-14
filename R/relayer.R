a7c5767415288b3e2ee14c1ef5eef152ad4f22bf
Claus Wilke <wilke@austin.utexas.edu>
2018-05-14 12:58:10 -0500

    add startup warning message

--- R/relayer.R
+++ R/relayer.R
@@ -5,3 +5,12 @@
 #' @import ggplot2
 #' @import rlang
 NULL
+
+
+# *************************************************
+#                     Setup
+# *************************************************
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Note: The package \"relayer\" is highly experimental. Use at your own risk.")
+}
